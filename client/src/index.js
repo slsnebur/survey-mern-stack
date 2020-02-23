f19f28f9c6a4ce49c546718d30754a7c718583a6
Karol Morawski <karol.morawski4@gmail.com>
2020-02-23 17:36:33 +0100

    updated client

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,73 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+// eslint-disable-next-line
+import { Button, DatePicker, version, Layout, Menu, Breadcrumb } from "antd";
+import "antd/dist/antd.css";
+import "./index.css";
+import axios from 'axios'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class UserList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: 'noelo',
+        users: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get('http://localhost:3001/api/users?page=0')
+            .then(res => {
+                this.setState({ users: res.data });
+                console.log(res.data);
+            }).catch(err => console.log(err));
+    }
+
+    render() {
+        return (
+            <ul>
+                { this.state.users[0]}
+            </ul>
+        )
+    }
+}
+// eslint-disable-next-line
+const { Header, Content, Footer } = Layout;
+
+/*ReactDOM.render(
+    <Layout className="layout">
+        <Header>
+            <div className="logo" />
+            <Menu
+                theme="dark"
+                mode="horizontal"
+                defaultSelectedKeys={['2']}
+                style={{ lineHeight: '64px' }}
+            >
+                <Menu.Item key="1">nav 1</Menu.Item>
+                <Menu.Item key="2">nav 2</Menu.Item>
+                <Menu.Item key="3">nav 3</Menu.Item>
+            </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+            <Breadcrumb style={{ margin: '16px 0' }}>
+                <Breadcrumb.Item>Home</Breadcrumb.Item>
+                <Breadcrumb.Item>List</Breadcrumb.Item>
+                <Breadcrumb.Item>App</Breadcrumb.Item>
+            </Breadcrumb>
+            <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+
+                <UserList/>
+
+            </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>,
+    document.getElementById("root")
+);
+ */
+
+ReactDOM.render(
+    <UserList/>,
+    document.getElementById("root")
+);
